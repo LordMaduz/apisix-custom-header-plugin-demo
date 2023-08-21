bba416bba00fe923804c66599144defb505ce274
LordMaduz <52396694+LordMaduz@users.noreply.github.com>
2023-08-21 13:55:20 +0800

    Create custom-header.lua

---
+++ custom-header.lua
@@ -0,0 +1,34 @@
+local require = require
+local core    = require("apisix.core")
+
+-- module define
+local plugin_name = "demo-plugin"
+
+-- plugin schema
+local plugin_schema = {
+    type = "object",
+    properties = {},
+    required = {},
+}
+
+local _M = {
+    version  = 0.1,            -- plugin version
+    priority = 0,              -- the priority of this plugin will be 0
+    name     = plugin_name,    -- plugin name
+    schema   = plugin_schema,  -- plugin schema
+}
+
+-- module interface for schema check
+-- @param `conf` user defined conf data
+-- @param `schema_type` defined in `apisix/core/schema.lua`
+-- @return <boolean>
+function _M.check_schema(conf, schema_type)
+    return core.schema.check(plugin_schema, conf)
+end
+
+-- module interface for header_filter phase
+function _M.header_filter(conf, ctx)
+  core.request.set_header(ctx, conf.header, conf.value)
+end
+
+return _M
